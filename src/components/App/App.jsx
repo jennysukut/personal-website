7e6af76336148c0c7f4b2a1fea8f383a090ff083
JennySukut <jennysukut@gmail.com>
2024-05-07 14:51:58 +0200

    Getting Pages to Deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef } from "react";
-import { Routes, Route } from "react-router-dom";
 import setIntersectionObservers from "../../utils/intersectionObserver";
 
 import "./App.css";
